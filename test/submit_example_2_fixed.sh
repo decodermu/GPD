4aab4aeb31bc21abbd8e0a78ce6e690d833950c6
decodermu <90942779+decodermu@users.noreply.github.com>
2024-01-20 16:04:44 +0800

    Update submit_example_2_fixed.sh

--- test/submit_example_2_fixed.sh
+++ test/submit_example_2_fixed.sh
@@ -2,7 +2,7 @@ source activate GPD
 input_pdb="../inputs/1tca.pdb"
 output_dir="../outputs/example_2_fixed"
 fiexed_sites="../inputs/fixed_sites.txt"
-python ../protein_GPD_run.py \
+python ../run_design.py \
         --pdb_file $input_pdb \
         --num_seq  2 \
         --out_folder $output_dir \
